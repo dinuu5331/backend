ecf23ead8be89f7c71eb54afa27c3c4d0e550161
dinuu5331 <dinuu5331@gmail.com>
2024-07-26 17:00:39 +0530

    create routes and controllers

--- src/app.js
+++ src/app.js
@@ -13,4 +13,15 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser)
+
+
+//router import
+
+import userRouter from "./routes/user.router.js"
+
+//router declaration
+
+app.use("../api/v1/users",userRouter)
+
+
 export {app}
